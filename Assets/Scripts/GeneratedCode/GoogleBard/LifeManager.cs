4841e7fd8ccd856022c157e4347f43d9a367a0b8
LivReTaRDIS <lv.zuhause@gmail.com>
2023-08-04 15:40:12 +0200

    [AI][Google Bard] added Generated Code

---
+++ Assets/Scripts/GeneratedCode/GoogleBard/LifeManager.cs
@@ -0,0 +1,49 @@
+namespace GeneratedCode.GoogleBard
+{
+    using UnityEngine;
+    public class LifeManager : MonoBehaviour
+    {
+        public int life = 3;
+
+        public UnityEvent onLifeIncrease;
+        public UnityEvent onLifeDecrease;
+        public UnityEvent onGameOver;
+
+        private void Awake()
+        {
+            // Subscribe to the life increase event
+            onLifeIncrease.AddListener(OnLifeIncrease);
+
+            // Subscribe to the life decrease event
+            onLifeDecrease.AddListener(OnLifeDecrease);
+
+            // Subscribe to the game over event
+            onGameOver.AddListener(OnGameOver);
+        }
+
+        private void OnLifeIncrease()
+        {
+            // Increase the life by one
+            life++;
+        }
+
+        private void OnLifeDecrease()
+        {
+            // Decrease the life by one
+            life--;
+
+            // If the life is zero, call the game over function
+            if (life == 0)
+            {
+                OnGameOver();
+            }
+        }
+
+        private void OnGameOver()
+        {
+            // Display a game over message
+            Debug.Log("Game Over!");
+        }
+    }
+
+}
