edbf31fcce1f5e8d069a9006b615a1353bf32bea
LivReTaRDIS <lv.zuhause@gmail.com>
2023-08-07 17:52:26 +0200

    [AI] [GTP 4.0] Generated code with additionla capeability of taking an game objecz

--- Assets/ExitTrigger.cs
+++ Assets/ExitTrigger.cs
@@ -7,20 +7,23 @@ using UnityEngine.Serialization;
 
 public class ExitTrigger : MonoBehaviour
 {
-    public UnityEvent exitTriggerEvent;
+    [System.Serializable]
+    public class GameObjectEvent : UnityEvent<GameObject> { }
+
+    public GameObjectEvent exitTriggerEvent;
 
     private void Awake()
     {
-        exitTriggerEvent.AddListener( DebugMessage);
+        exitTriggerEvent.AddListener(DebugMessage);
     }
 
-    private void DebugMessage()
+    private void DebugMessage(GameObject obj)
     {
-        Debug.Log("Object has exited the trigger");
+        Debug.Log(obj.name + " has exited the trigger");
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(exitTriggerEvent != null) exitTriggerEvent.Invoke();
+        if(exitTriggerEvent != null) exitTriggerEvent.Invoke(other.gameObject);
     }
 }
