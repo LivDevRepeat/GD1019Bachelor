5e0dae6f6fc8f4354490b0966f5d42c84446b293
LivReTaRDIS <lv.zuhause@gmail.com>
2023-08-07 11:35:49 +0200

    [Manual Fix] added Missing dependencies

--- Assets/Scripts/GeneratedCode/GoogleBard/LifeManager.cs
+++ Assets/Scripts/GeneratedCode/GoogleBard/LifeManager.cs
@@ -1,49 +1,55 @@
-namespace GeneratedCode.GoogleBard
-{
-    using UnityEngine;
-    public class LifeManager : MonoBehaviour
+#region Manual Fix
+    using UnityEngine.Events; // Added the missing dependencies
+#endregion
+
+#region Generated Code
+    namespace GeneratedCode.GoogleBard
     {
-        public int life = 3;
-
-        public UnityEvent onLifeIncrease;
-        public UnityEvent onLifeDecrease;
-        public UnityEvent onGameOver;
-
-        private void Awake()
+        using UnityEngine;
+        public class LifeManager : MonoBehaviour
         {
-            // Subscribe to the life increase event
-            onLifeIncrease.AddListener(OnLifeIncrease);
+            public int life = 3;
 
-            // Subscribe to the life decrease event
-            onLifeDecrease.AddListener(OnLifeDecrease);
+            public UnityEvent onLifeIncrease;
+            public UnityEvent onLifeDecrease;
+            public UnityEvent onGameOver;
 
-            // Subscribe to the game over event
-            onGameOver.AddListener(OnGameOver);
-        }
-
-        private void OnLifeIncrease()
-        {
-            // Increase the life by one
-            life++;
-        }
-
-        private void OnLifeDecrease()
-        {
-            // Decrease the life by one
-            life--;
-
-            // If the life is zero, call the game over function
-            if (life == 0)
+            private void Awake()
             {
-                OnGameOver();
+                // Subscribe to the life increase event
+                onLifeIncrease.AddListener(OnLifeIncrease);
+
+                // Subscribe to the life decrease event
+                onLifeDecrease.AddListener(OnLifeDecrease);
+
+                // Subscribe to the game over event
+                onGameOver.AddListener(OnGameOver);
+            }
+
+            private void OnLifeIncrease()
+            {
+                // Increase the life by one
+                life++;
+            }
+
+            private void OnLifeDecrease()
+            {
+                // Decrease the life by one
+                life--;
+
+                // If the life is zero, call the game over function
+                if (life == 0)
+                {
+                    OnGameOver();
+                }
+            }
+
+            private void OnGameOver()
+            {
+                // Display a game over message
+                Debug.Log("Game Over!");
             }
         }
 
-        private void OnGameOver()
-        {
-            // Display a game over message
-            Debug.Log("Game Over!");
-        }
     }
-
-}
+#endregion
